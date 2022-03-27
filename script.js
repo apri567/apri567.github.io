bb91c1ec6fea75314c3972d5f10ee6c8994956cf
apri567 <92333530+apri567@users.noreply.github.com>
2022-03-27 23:03:48 +0300

    Добавлены комментарии к коду

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-'use strict';
+'use strict'; // Включаем "строгий режим"
 
+// Сохраняем значение HTML тегов в переменные
 const resultTag = document.getElementById('result');
 const resultParagraph = document.getElementById('resultParagraph');
 const destroyBtn = document.getElementById('destroyBtn');
@@ -10,66 +11,72 @@ const symbolParagraph = document.getElementById('symbolParagraph');
 const backBtn = document.getElementById('backBtn');
 const sumBtn = document.getElementById('sumBtn');
 
-function destroy () { // 2
-    const textVal = textInput.value;
-    const symbolVal = symbolInput.value;
-    let result = '';
-    for (let i of textVal) {
-        if (i != symbolVal) {
-            result += i;
+// Функция, удаляющая символ из текста:
+function destroy () {
+    const textVal = textInput.value; // Сохраняем значение из поля ввода текста
+    const symbolVal = symbolInput.value; // Сохраняем значение из поля ввода символа
+    let result = ''; // Создадим переменную, в будущем хранящую результат удаления символа из текста
+    for (let i of textVal) { // Перебираем символы в textVal
+        if (i != symbolVal) { // Если символ не равен символу, который нужно удалить:
+            result += i; // Добавляем символ в переменную, хранящую результат
         }
-        else {
-            result += ' ';
+        else { // Иначе:
+            result += ' '; // Добавляем пробел вместо символа, который нужно пропустить
         }
     }
-    return result;
+    return result; // Возвращаем результат
 }
 
-function displayResult () { // 1
+// Функция отображения результата, запускается кнопкой УНИЧТОЖИТЬ:
+function displayResult () {
+    // Если поле ввода текста не пустое:
     if (textInput.value != '') {
-        resultTag.style.display = 'block';
-        resultTag.innerHTML = destroy();
-        resultParagraph.style.display = 'block';
-        destroyBtn.style.display = 'none';
-        textInput.style.display = 'none';
-        symbolInput.style.display = 'none';
-        textParagraph.style.display = 'none';
-        symbolParagraph.style.display = 'none';
-        backBtn.style.display = 'block';
-        displaySumBtn();
+        resultTag.style.display = 'block'; // Отобразить тег с результатом
+        resultTag.innerHTML = destroy(); // Изменить содержание тега с результатом
+        resultParagraph.style.display = 'block'; // Отобразить текст "Результат:"
+        destroyBtn.style.display = 'none'; // Скрыть кнопку УНИЧТОЖИТЬ
+        textInput.style.display = 'none'; // Скрыть поле ввода текста
+        symbolInput.style.display = 'none'; // Скрыть поле ввода символа, который нужно удалить
+        textParagraph.style.display = 'none'; // Скрыть надпись "Введите текст:"
+        symbolParagraph.style.display = 'none'; // Скрыть надпись "Введите символ:"
+        backBtn.style.display = 'block'; // Показать кнопку "Назад"
+        displaySumBtn(); // Запустить функцию по отображению кнопки "+"
     }
 }
 
+// Функция, скрывающая результат и возвращающая всё в начальное положение, срабатывает при нажатии кнопки "Назад":
 function back () {
-    resultTag.style.display = 'none';
-    resultParagraph.style.display = 'none';
-    destroyBtn.style.display = 'block';
-    textInput.style.display = 'inline';
-    symbolInput.style.display = 'inline';
-    textParagraph.style.display = 'block';
-    symbolParagraph.style.display = 'block';
-    backBtn.style.display = 'none';
-    sumBtn.style.display = 'none';
+    resultTag.style.display = 'none'; // Скрыть результат
+    resultParagraph.style.display = 'none'; // Скрыть надпись "Результат:"
+    destroyBtn.style.display = 'block'; // Показать кнопку УНИЧТОЖИТЬ
+    textInput.style.display = 'inline'; // Показать поле ввода текста
+    symbolInput.style.display = 'inline'; // Показать поле ввода символа, который нужно удалить
+    textParagraph.style.display = 'block'; // Показать надпись "Введите текст:"
+    symbolParagraph.style.display = 'block'; // Показать надпись "Введите символ:"
+    backBtn.style.display = 'none'; // Скрыть кнопку "Назад"
+    sumBtn.style.display = 'none'; // Скрыть кнопку "+"
 }
 
-function displaySumBtn () { // 3
-    let resultList = resultTag.innerHTML.split(' ');
-    let letterInList = false;
-    for (let i of resultList) {
-        if (isNaN(+i) == true) {
-            letterInList = true;
+// Функция, решающая, нужно ли показывать кнопку "+", и отображающая её:
+function displaySumBtn () {
+    let resultList = resultTag.innerHTML.split(' '); // Разделяем готовый текст по пробелам в список
+    let letterInList = false; // Переменная, определяющая, есть ли буква в списке
+    for (let i of resultList) { // Перебираем список
+        if (isNaN(+i) == true) { // Если символ невозможно преобразовать в число (если буква в списке)
+            letterInList = true; // Изменяем значение переменной на true
         }
     }
-    if (letterInList == false) {
-        sumBtn.style.display = 'block';
+    if (letterInList == false) { // Если букв нет в списке
+        sumBtn.style.display = 'block'; // Отобразить кнопку "+"
     }
 }
 
+// Функция, которая суммирует все разделённые числа в списке, срабатывает при нажатии кнопки "+":
 function sum () {
-    let resultList = resultTag.innerHTML.split(' ');
-    let sumResult = 0;
-    for (let i of resultList) {
-        sumResult += +i;
+    let resultList = resultTag.innerHTML.split(' '); // Разделяем готовый текст по пробелам в список
+    let sumResult = 0; // Создаём переменную, которая будет хранить результат суммирования чисел
+    for (let i of resultList) { // Перебираем список
+        sumResult += +i; // Суммируем числа
     }
-    resultTag.innerHTML = sumResult.toString();
+    resultTag.innerHTML = sumResult.toString(); // Отображаем результат на экран
 }
